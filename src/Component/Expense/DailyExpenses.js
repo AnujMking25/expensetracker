2c1cd3baf69d7ebd7c7b7629afbaa5a0cdba35ee
AnujMking25 <anuj170499@gmail.com>
2023-04-20 01:27:51 +0530

    update

--- src/Component/Expense/DailyExpenses.js
+++ src/Component/Expense/DailyExpenses.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './DailyExpenses.module.css'
 const DailyExpenses = () => {
+    const Inputmoney=useRef();
+    const InputDescription=useRef()
+    const InputCategory=useRef()
+    // const[list ,setlist]=useState();
+    let items=[];
+    
+const data=<ul>
+   { items.map(item=><li>{item.money}=={item.description}=={item.category}</li>)}
+</ul>
+console.log(data);
+    let url='https://expense-tracker-3983f-default-rtdb.firebaseio.com/Expense.json'
+   async function onExpensesHandler(e){
+        e.preventDefault();
+        // console.log(InputCategory.current.value)
+        const EnteredMoney=Inputmoney.current.value;
+        const EnteredDescription=InputDescription.current.value;
+        const EnteredCategory=InputCategory.current.value;
+
+// *********************** Post Request Api ***********==> START HERE<==*****************
+       try {
+        const PostApi=await fetch(url,{
+            method:'POST',
+            body:JSON.stringify({
+                money:EnteredMoney,
+                description:EnteredDescription,
+                category:EnteredCategory
+            }),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        if(PostApi.ok){
+            alert("successfully")
+            
+            return {items:items.push({ money:EnteredMoney,
+                description:EnteredDescription,
+                category:EnteredCategory})
+            }    
+        
+        }
+       } catch (error) {
+        alert(error.message)
+       }
+// *********************** Post Request Api ***********==> END HERE<==*****************
+    }
   return (
     <div className={classes.maindiv}>
         <h1>Daily Expenses</h1>
-    <form>
+    <form onSubmit={onExpensesHandler}>
         <table>
             <tbody>
                 <tr>
@@ -13,13 +58,13 @@ const DailyExpenses = () => {
                     <th>Category</th>
                 </tr>
                 <tr>
-                    <td><input type='number'/></td>
-                    <td><input type='text'/></td>
+                    <td><input type='number' ref={Inputmoney}/></td>
+                    <td><input type='text' ref={InputDescription}/></td>
                     <td>
-                        <select name='Category' id='Tcategory'>
-                            <option >Food</option>
-                            <option>Petrol</option>
-                            <option>Salary</option>
+                        <select name='Category' id='Tcategory' ref={InputCategory}>
+                            <option value='Food'>Food</option>
+                            <option value='Petrol'>Petrol</option>
+                            <option value='Salary'>Salary</option>
                         </select>
                     </td>
                 </tr>
@@ -27,6 +72,7 @@ const DailyExpenses = () => {
         </table>
         <button>submit</button>
     </form>
+    {data}
     </div>
   )
 }
